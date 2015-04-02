5107de175afc149ee4322759cdcad504a2badcdc
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2015-04-02 10:43:03 -0700

    Image/header updates

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <?php use Roots\Sage\Nav\NavWalker; ?>
 
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner navbar navbar-default navbar-static-top no-side-margin" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
